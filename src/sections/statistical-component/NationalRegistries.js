b187469ab96cc04bc1c1292c7b68bdfb80f9aec0
v08nike <teddan817@gmail.com>
2023-03-17 08:28:34 -0700

    Fix; applyed exception of code

--- src/sections/statistical-component/NationalRegistries.js
+++ src/sections/statistical-component/NationalRegistries.js
@@ -17,9 +17,10 @@ NationalRegistries.propTypes = {
   data: PropTypes.object,
 };
 
-export default function NationalRegistries({data}) {
+export default function NationalRegistries({ data }) {
   const theme = useTheme();
-  const {countOfFilesBySanctioningBody, forestAndWildlifeRegents, forestPlantations, installAreaOfForestPlantations} = data;
+  const { countOfFilesBySanctioningBody, forestAndWildlifeRegents, forestPlantations, installAreaOfForestPlantations } =
+    data;
 
   return (
     <Grid container spacing={2}>
@@ -29,60 +30,69 @@ export default function NationalRegistries({data}) {
       <Grid item xs={12}>
         <CardHeader title={'Plantaciones forestales'} />
       </Grid>
-      <Grid item xs={12} md={4}>
-        <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} sx={{ height: '455px' }}>
-          <NationalRegistriesSummary
-            title={forestPlantations.title}
-            icon={'eva:diagonal-arrow-left-down-fill'}
-            percent={forestPlantations.percent}
-            total={forestPlantations.total}
-            chartData={forestPlantations.data}
-          />
-        </Stack>
-      </Grid>
-      <Grid item xs={12} md={8}>
-        <NationalRegistriesChart
-          title="SUPERFICIE INSTALADA DE PLANTACIONES FORESTALES (HA)"
-          chartLabels={installAreaOfForestPlantations.chartLabels}
-          chartData={installAreaOfForestPlantations.chartData}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <CardHeader title={forestAndWildlifeRegents.title} />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <Card sx={{ p: 2 }}>
-          <Stack direction="row" spacing={2}>
-            {forestAndWildlifeRegents.data.map((item, index) => (
-              <Stack key={index} direction="column" justifyContent="space-between" sx={{ pt: 2 }}>
-                <Typography variant="h6" component="h6" textAlign="center">
-                  {item.title}
-                </Typography>
-                <Typography
-                  variant="h5"
-                  component="h5"
-                  textAlign="center"
-                  sx={{ pt: 1, color: theme.palette.secondary.main }}
-                >
-                  {item.value}
-                </Typography>
-              </Stack>
-            ))}
+      {forestPlantations && (
+        <Grid item xs={12} md={4}>
+          <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} sx={{ height: '455px' }}>
+            <NationalRegistriesSummary
+              title={forestPlantations.title}
+              icon={'eva:diagonal-arrow-left-down-fill'}
+              percent={forestPlantations.percent}
+              total={forestPlantations.total}
+              chartData={forestPlantations.data}
+            />
           </Stack>
-        </Card>
-      </Grid>
-
+        </Grid>
+      )}
+      {installAreaOfForestPlantations && (
+        <Grid item xs={12} md={8}>
+          <NationalRegistriesChart
+            title="SUPERFICIE INSTALADA DE PLANTACIONES FORESTALES (HA)"
+            chartLabels={installAreaOfForestPlantations.chartLabels}
+            chartData={installAreaOfForestPlantations.chartData}
+          />
+        </Grid>
+      )}
+      {forestAndWildlifeRegents && (
+        <Grid item xs={12}>
+          <CardHeader title={forestAndWildlifeRegents.title} />
+        </Grid>
+      )}
+      {forestAndWildlifeRegents && (
+        <Grid item xs={12} md={6}>
+          <Card sx={{ p: 2 }}>
+            <Stack direction="row" spacing={2}>
+              {forestAndWildlifeRegents.data.map((item, index) => (
+                <Stack key={index} direction="column" justifyContent="space-between" sx={{ pt: 2 }}>
+                  <Typography variant="h6" component="h6" textAlign="center">
+                    {item.title}
+                  </Typography>
+                  <Typography
+                    variant="h5"
+                    component="h5"
+                    textAlign="center"
+                    sx={{ pt: 1, color: theme.palette.secondary.main }}
+                  >
+                    {item.value}
+                  </Typography>
+                </Stack>
+              ))}
+            </Stack>
+          </Card>
+        </Grid>
+      )}
       <Grid item xs={12}>
         <CardHeader title={'Infractores'} />
       </Grid>
-      <Grid item xs={12}>
-        <CustomAppChart
-          title={countOfFilesBySanctioningBody.title}
-          chartData={countOfFilesBySanctioningBody.chartData}
-          colors={[theme.palette.chart.lightBlue[1]]}
-          dataLabelsOffsetX={40}
-        />
-      </Grid>
+      {countOfFilesBySanctioningBody && (
+        <Grid item xs={12}>
+          <CustomAppChart
+            title={countOfFilesBySanctioningBody.title}
+            chartData={countOfFilesBySanctioningBody.chartData}
+            colors={[theme.palette.chart.lightBlue[1]]}
+            dataLabelsOffsetX={40}
+          />
+        </Grid>
+      )}
     </Grid>
   );
 }
